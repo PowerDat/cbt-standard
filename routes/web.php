3401f37557a34a923e0cc0d773fd1cedf7c51f5e
kid009 <pongpoom1985@gmail.com>
2024-06-13 13:25:47 +0700

    กำหนดสิทธิ์บทบาท

--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,8 @@ use App\Http\Controllers\EvaluateController;
 use App\Http\Controllers\PartTypeController;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\PartIndexController;
-use App\Http\Controllers\PartDetailController;
-use App\Http\Controllers\PartTargetController;
 use App\Http\Controllers\UserProfileController;
-use App\Http\Controllers\PartTargetSubController;
 use App\Http\Controllers\PermissionController;
-use App\Http\Controllers\PostController;
-
-// Route::get('/', function () {
-//     return redirect()->route('login');
-// });
 
 Route::get('/', [AuthController::class, 'login'])->name('auth.login');
 Route::post('auth/post-login', [AuthController::class, 'postLogin'])->name('auth.post-login'); 
@@ -46,21 +37,6 @@ Route::middleware(['auth'])->group(function(){
     Route::get('evaluate/getPartType/{part_type_id?}', [EvaluateController::class, 'getPartType'])->name('evaluate.getPartType');
     Route::post('evaluate/save-community', [EvaluateController::class, 'saveCommunity'])->name('evaluate.save-community');
     
-    //สร้างจากหน้า part target
-    Route::resource('part-target', PartTargetController::class);
-    Route::get('part-target/createByPartId/{id?}', [PartTargetController::class, 'createByPartId'])->name('part-target.createByPartId');
-    Route::post('part-target/delete', [PartTargetController::class, 'delete'])->name('part-target.delete');
-    Route::resource('part-target-sub', PartTargetSubController::class);
-    Route::resource('part-index', PartIndexController::class);
-    Route::get('part-index/create-by-id/{id?}', [PartIndexController::class, 'createById'])->name('part-index.create-by-id');
-
-    //รายละเอียดของแต่ละด้าน
-    Route::get('part-detail/createByTargetId/{id?}', [PartDetailController::class, 'createByTargetId'])->name('part-detail.createByTargetId');
-    Route::post('part-detail/fetchPartTargetById', [PartDetailController::class, 'fetchPartTargetById'])->name('part-detail.fetchPartTargetById');
-    Route::resource('part-detail', PartDetailController::class);
-    Route::post('part-detail/saveTargetSub', [PartDetailController::class, 'saveTargetSub'])->name('part-detail.saveTargetSub');
-    Route::post('part-detail/delete', [PartDetailController::class, 'delete'])->name('part-detail.delete');
-    
     //report
     Route::get('report', [ReportController::class, 'index'])->name('report.index');
     Route::get('report/self', [ReportController::class, 'self'])->name('report.self');
@@ -70,7 +46,21 @@ Route::middleware(['auth'])->group(function(){
     Route::get('report/part-third', [ReportController::class, 'partThird'])->name('report.part-third');
     Route::get('report/part-fourth', [ReportController::class, 'partFourth'])->name('report.part-fourth');
     Route::get('report/part-fifth', [ReportController::class, 'partFifth'])->name('report.part-fifth');
+});
 
+Route::middleware(['auth', 'role:researcher'])->group(function(){
+    //จัดการผู้ใช้งาน
+    Route::resource('user', UserController::class);
+    Route::post('user/delete', [UserController::class, 'delete'])->name('user.delete');
+    Route::get('user/change-password/{id?}', [UserController::class, 'changePassword'])->name('user.change-password');
+    Route::post('user/save-change-password', [UserController::class, 'saveChangePassword'])->name('user.save-change-password');
+
+    Route::get('user-profile/createById/{id?}', [UserProfileController::class, 'createById'])->name('user-profile.createById');
+    Route::resource('user-profile', UserProfileController::class);
+    Route::post('user-profile/delete', [UserProfileController::class, 'delete'])->name('user-profile.delete');
+});
+
+Route::middleware(['auth', 'role:administrator'])->group(function(){
     //ประเภทเกณฑ์มาตรฐาน
     Route::resource('part-type', PartTypeController::class);
     Route::post('part-type/delete', [PartTypeController::class, 'delete'])->name('part-type.delete');
@@ -88,6 +78,7 @@ Route::middleware(['auth'])->group(function(){
     Route::resource('permission', PermissionController::class);
     Route::post('permission/delete', [PermissionController::class, 'delete'])->name('permission.delete');
 
+    //จัดการผู้ใช้งาน
     Route::resource('user', UserController::class);
     Route::post('user/delete', [UserController::class, 'delete'])->name('user.delete');
     Route::get('user/change-password/{id?}', [UserController::class, 'changePassword'])->name('user.change-password');
@@ -102,39 +93,4 @@ Route::middleware(['auth'])->group(function(){
     Route::resource('sub-menu', SubMenuController::class);
 
     Route::get('logs', [\Rap2hpoutre\LaravelLogViewer\LogViewerController::class, 'index'])->name('logs');
-
 });
-
-// Route::middleware(['auth', 'role:administrator'])->group(function(){
-//     //ประเภทเกณฑ์มาตรฐาน
-//     Route::resource('part-type', PartTypeController::class);
-//     Route::post('part-type/delete', [PartTypeController::class, 'delete'])->name('part-type.delete');
-//     //ข้อมูลเกณฑ์มาตรฐาน
-//     Route::get('part/createByPartTypeId/{id?}', [PartController::class, 'createByPartTypeId'])->name('part.createByPartTypeId');
-//     Route::post('part/delete', [PartController::class, 'delete'])->name('part.delete');
-//     Route::resource('part', PartController::class);
-
-//     // ตั้งค่าระบบ
-//     Route::resource('role', RoleController::class);
-//     Route::post('role/delete', [RoleController::class, 'delete'])->name('role.delete');
-
-//     Route::post('role/{role}/assign-permission', [RoleController::class, 'assignPermission'])->name('role.assign-permission');
-
-//     Route::resource('permission', PermissionController::class);
-//     Route::post('permission/delete', [PermissionController::class, 'delete'])->name('permission.delete');
-
-//     Route::resource('user', UserController::class);
-//     Route::post('user/delete', [UserController::class, 'delete'])->name('user.delete');
-//     Route::get('user/change-password/{id?}', [UserController::class, 'changePassword'])->name('user.change-password');
-//     Route::post('user/save-change-password', [UserController::class, 'saveChangePassword'])->name('user.save-change-password');
-
-//     Route::get('user-profile/createById/{id?}', [UserProfileController::class, 'createById'])->name('user-profile.createById');
-//     Route::resource('user-profile', UserProfileController::class);
-//     Route::post('user-profile/delete', [UserProfileController::class, 'delete'])->name('user-profile.delete');
-
-//     Route::resource('menu', MenuController::class);
-//     Route::get('sub-menu/createByMenuId/{id?}', [SubMenuController::class, 'createByMenuId'])->name('sub-menu.createByMenuId');
-//     Route::resource('sub-menu', SubMenuController::class);
-
-//     Route::get('logs', [\Rap2hpoutre\LaravelLogViewer\LogViewerController::class, 'index'])->name('logs');
-// });
