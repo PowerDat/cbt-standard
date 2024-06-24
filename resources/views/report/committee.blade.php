78447c572299ccabf4bff21728f27a27edac7195
kid009 <pongpoom1985@gmail.com>
2024-06-24 13:32:31 +0700

    รายงานกรรมการ

--- resources/views/report/committee.blade.php
+++ resources/views/report/committee.blade.php
@@ -8,12 +8,15 @@
             <div class="col-sm-12">
                 <ol class="breadcrumb">
                     <li class="breadcrumb-item">สรุปรายงาน</li>
-                    <li class="breadcrumb-item">
+                    <li class="breadcrumb-item active">
                         @if (session()->has('community_name'))
                         {{session()->get('community_name')}}
                         @endif
+
+                        @if (session()->has('session_community_by_select_option'))
+                        {{session()->get('session_community_by_select_option')}}
+                        @endif
                     </li>
-                    <li class="breadcrumb-item active">กรรมการประเมิน</li>
                 </ol>
             </div>
         </div>
@@ -22,94 +25,92 @@
 
 <!-- content -->
 <div class="container-fluid">
-
+    
     <div class="row">
-        <div class="col-sm-12 col-xl-6 xl-100">
+        <div class="col-sm-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col">
+                            <div class="mb-3">
+                                <label class="form-label">ชุมชนที่ประเมิน</label>
+                                <select  class="form-control" id="evaluate_community" >
+                                    <option value="" selected disabled>เลือกชุมชน</option>
+                                    @for ($i=0; $i < count($response_community_by_api); $i++)
+                                        @foreach ($community as $comm)
+                                            @if ($response_community_by_api[$i]['community_id'] == $comm->community_id)
+                                            <option value="{{$comm->community_id}}"
+                                                @if (session()->has('session_community_id_by_select_option')) 
+                                                @if (session()->get('session_community_id_by_select_option') == $comm->community_id)
+                                                selected
+                                                @endif
+                                            @endif    
+                                            >
+                                                {{$response_community_by_api[$i]['community_name']}}
+                                            </option>
+                                            @endif
+                                        @endforeach
+                                    @endfor
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="col-sm-12">
             <div class="card">
                 <div class="card-header pb-0">
-                    <h5>สรุปรายงานการประเมินตามเกณฑ์มาตรฐาน : @if (session()->has('community_name'))
-                        {{session()->get('community_name')}} @endif</h5>
+                    <h5>สรุปรายงานการประเมินตามเกณฑ์มาตรฐาน : @if (session()->has('community_name')) {{session()->get('community_name')}} @endif</h5>
                 </div>
                 <div class="card-body">
                     <div class="row">
                         <div class="col">
-                            <a class="btn btn-primary " href="{{route('report.self')}}">
+                            <a class="btn btn-light" href="{{route('report.self-assessment')}}">
                                 ประเมินตนเอง
                             </a>
-                            <a class="btn btn-light active" href="{{route('report.committee')}}">
+                            <a class="btn btn-primary active" href="{{route('report.evaluation-committee')}}">
                                 กรรมการประเมิน
                             </a>
-                            <a class="btn btn-light" href="">
+                            {{-- <a class="btn btn-light" href="">
                                 สรุปผลการประเมิน
-                            </a>
+                            </a> --}}
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
-    <div class="col-sm-12 col-xl-6 xl-100">
-        <div class="card">
-
-            <div class="card-body">
-                <ul class="nav nav-tabs nav-primary" id="pills-warningtab" role="tablist">
-                    <li class="nav-item">
-                        <a class="nav-link active" id="pills-1-tab" data-bs-toggle="pill" href="#pills-1" role="tab"
-                            aria-controls="pills-1" aria-selected="true">
-                            ด้าน 1
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" id="pills-2-tab" data-bs-toggle="pill" href="#pills-2" role="tab"
-                            aria-controls="pills-2" aria-selected="false">
-                            ด้าน 2
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" id="pills-3-tab" data-bs-toggle="pill" href="#pills-3" role="tab"
-                            aria-controls="pills-3" aria-selected="false">
-                            ด้าน 3
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" id="pills-4-tab" data-bs-toggle="pill" href="#pills-4" role="tab"
-                            aria-controls="pills-4" aria-selected="false">
-                            ด้าน 4
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" id="pills-5-tab" data-bs-toggle="pill" href="#pills-5" role="tab"
-                            aria-controls="pills-5" aria-selected="false">
-                            ด้าน 5
-                        </a>
-                    </li>
-                </ul>
-
-                <div class="tab-content" id="pills-warningtabContent">
-
-                    {{-- /* ----- ด้าน 1 ----- */ --}}
-                    @include('report.part-first')
-
-                    {{-- /* ----- ด้าน 2 ----- */ --}}
-                    @include('report.part-second')
-
-                    {{-- /* ----- ด้าน 3 ----- */ --}}
-                    @include('report.part-third')
-
-                    {{-- /* ----- ด้าน 4 ----- */ --}}
-                    @include('report.part-fourth')
-
-                    {{-- /* ----- ด้าน 5 ----- */ --}}
-                    @include('report.part-fifth')
-
-                </div>
-            </div>
-        </div>
-    </div>
+    
 </div>
 @endsection
 
 @push('scripts')
+<script>
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
 
+    $(document).ready(function(){
+
+        $('#evaluate_community').change(function(){           
+            $.ajax({
+                type: 'post',
+                url: "{{ route('evaluate.save-community') }}",
+                data: {
+                    evaluate_community:  $('#evaluate_community').val()
+                },
+                success: (response) => {
+                    if(response.status == 1){
+                        window.location.reload();
+                    }
+                },
+            });
+        });
+
+    });
+</script>
 @endpush
---
+++ resources/views/report/committee/index.blade.php
@@ -0,0 +1,157 @@
+@extends('layouts.master')
+
+@section('content')
+<!-- breadcrumb -->
+<div class="container-fluid">
+    <div class="page-header">
+        <div class="row">
+            <div class="col-sm-12">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item">สรุปรายงาน</li>
+                    <li class="breadcrumb-item active">
+                        @if (session()->has('community_name'))
+                        {{session()->get('community_name')}}
+                        @endif
+                    </li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- content -->
+<div class="container-fluid">
+    <div class="row">
+
+        <div class="col-sm-12 col-xl-6 xl-100">
+            <div class="card">
+                <div class="card-header pb-0">
+                    <h5>สรุปรายงานการประเมินตามเกณฑ์มาตรฐาน : @if (session()->has('community_name')) {{session()->get('community_name')}} @endif</h5>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col">
+                            <a class="btn btn-primary active" href="{{route('report.committee.index')}}">
+                                ชุมชนที่ประเมิน
+                            </a>
+                            <a class="btn btn-light" href="{{route('report.committee.summary')}}">
+                                สรุปผลการประเมิน
+                            </a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-12 col-xl-6 xl-100">
+            <div class="card">
+    
+                <div class="card-body">
+
+                    <form method="POST" action="{{route('report.committee.get-result')}}">
+                        @csrf
+                        <div class="row">
+                            <div class="col-sm-2">
+                                <label class="form-label">ชุมชน</label>
+                            </div>
+                            <div class="col-sm-8">
+                                <select  class="form-control" id="community" name="community">
+                                    <option value="" selected disabled>เลือกชุมชน</option>
+                                    @foreach ($community as $item)
+                                        <option value="{{$item->community_id}}" 
+                                        @if (session()->has('session_community'))
+                                            @if (session()->get('session_community') == $item->community_id)
+                                                selected
+                                            @endif
+                                        @endif
+                                        >{{$item->community_name}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="col-sm-2">
+                                <button type="submit" class="btn btn-primary">ค้นหา</button>
+                            </div>
+                        </div>
+                    </form>
+                    
+                    @if (isset($parts))
+                    <div class="row mt-5">
+                        <div class="col-sm-12">
+                            <ul class="nav nav-tabs nav-primary" id="pills-warningtab" role="tablist">
+                                <li class="nav-item">
+                                    <a class="nav-link active" id="pills-1-tab" data-bs-toggle="pill" href="#pills-1"
+                                        role="tab" aria-controls="pills-1" aria-selected="true">
+                                        ด้าน 1
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" id="pills-2-tab" data-bs-toggle="pill" href="#pills-2"
+                                        role="tab" aria-controls="pills-2" aria-selected="false">
+                                        ด้าน 2
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" id="pills-3-tab" data-bs-toggle="pill" href="#pills-3"
+                                        role="tab" aria-controls="pills-3" aria-selected="false">
+                                        ด้าน 3
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" id="pills-4-tab" data-bs-toggle="pill" href="#pills-4"
+                                        role="tab" aria-controls="pills-4" aria-selected="false">
+                                        ด้าน 4
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" id="pills-5-tab" data-bs-toggle="pill" href="#pills-5"
+                                        role="tab" aria-controls="pills-5" aria-selected="false">
+                                        ด้าน 5
+                                    </a>
+                                </li>
+                            </ul>
+
+                            <div class="tab-content" id="pills-warningtabContent">
+
+                                {{-- /* ----- ด้าน 1 ----- */ --}}
+                                @include('report.part-first')
+
+                                {{-- /* ----- ด้าน 2 ----- */ --}}
+                                @include('report.part-second')
+
+                                {{-- /* ----- ด้าน 3 ----- */ --}}
+                                @include('report.part-third')
+
+                                {{-- /* ----- ด้าน 4 ----- */ --}}
+                                @include('report.part-fourth')
+
+                                {{-- /* ----- ด้าน 5 ----- */ --}}
+                                @include('report.part-fifth')
+
+                            </div>
+                        </div>
+                    </div>
+                    @endif
+    
+                    
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+@endsection
+
+@push('scripts')
+<script>
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+
+    $(document).ready(function(){
+
+    });
+</script>
+@endpush
